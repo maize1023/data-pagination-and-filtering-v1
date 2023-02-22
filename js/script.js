51a74278772aeb5349e90810b3cce0c4b3f06851
Daiki Iwata <direction2423@gmail.com>
2023-02-22 15:59:15 -0800

    create showPage function

--- js/script.js
+++ js/script.js
@@ -5,26 +5,30 @@ FSJS Project 2 - Data Pagination and Filtering
 
 
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+// 1ページに対して、9人の生徒を表示するファンクション
+function showPage(list, page){
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = "";
 
+   for(let i = 0; i < list.length; i++){
+      if(startIndex <= i && i < endIndex){
+         const studentItem =`
+         <li class="student-item cf">
+           <div class="student-details">
+             <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
+             <h3>${list[i].name.first} ${list[i].name.last}</h3>
+             <span class="email">${list[i].email}</span>
+           </div>
+           <div class="joined-details">
+             <span class="date">Joined ${list[i].registered.date}</span>
+           </div>
+         </li>
+         `;
+         studentList.insertAdjacentHTML('beforeend', studentItem);
+      }
+   }
+}
 
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
-
-
-// Call functions
+showPage(data, 1);
